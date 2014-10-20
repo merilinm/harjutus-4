96146dcb2190bc0f387eb9af046a750cbe02aa48
merilinm <merilin.metsamaa@khk.ee>
2014-10-20 16:24:38 +0300

    Add integer and float calculations

--- index.php
+++ index.php
@@ -6,6 +6,36 @@
 </head>
 
 <body>
-    <h1></h1>
+    <h2>Arvutamine murd-ja täisarvudega</h2>
+    <?php
+    $integer = 3;
+    echo $float = 3.14;
+    echo "<br>";
+
+    echo $float + 7;
+    echo "<br>";
+
+	echo "4" / "3";    
+    ?>
+    
+    <h2>Murdarvude ümardamine</h2>
+    <?php
+    $source = round($float, 1);
+    echo "<p>{$source}</p>";
+ 
+    $ceil = ceil($float);
+    echo "<p>{$ceil}</p>";
+
+    $floor = floor($float);
+    echo "<p>{$floor}</p>";
+    ?>
+
+    <h2>Andmetüübi funktsioonid</h2>
+    <?php echo "Kas {$integer} on integer? " . is_int($integer); ?><br>
+    <?php echo "Kas {$float} on integer? " . is_int($float); ?><br>
+    <?php echo "Kas {$integer} on float? " . is_float($integer); ?><br>
+    <?php echo "Kas {$float} on float? " . is_float($float); ?><br>
+    <?php echo "Kas {$integer} on number? " . is_numeric($integer); ?><br>
+    <?php echo "Kas {$float} on number? " . is_numeric($float); ?><br>
 </body>
 </html>
