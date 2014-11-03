94c56d37c3771fc8d49ba2bfa6749c02911a50cc
merilinm <merilin.metsamaa@khk.ee>
2014-11-03 15:51:29 +0200

    Add Associative arrays

--- index.php
+++ index.php
@@ -22,10 +22,8 @@
     <?php
     $source = round($float, 1);
     echo "<p>{$source}</p>";
- 
-    $ceil = ceil($float);
+     $ceil = ceil($float);
     echo "<p>{$ceil}</p>";
-
     $floor = floor($float);
     echo "<p>{$floor}</p>";
     ?>
@@ -37,5 +35,51 @@
     <?php echo "Kas {$float} on float? " . is_float($float); ?><br>
     <?php echo "Kas {$integer} on number? " . is_numeric($integer); ?><br>
     <?php echo "Kas {$float} on number? " . is_numeric($float); ?><br>
+
+    <h2>Jadad (array)</h2>
+    <?php
+    $numbers = array(4,8,15,16,23,42);
+    echo $numbers[0];
+    echo "<br>";
+    echo $numbers[1];
+    echo "<br>";
+    echo $numbers[2];
+    echo "<br>";
+    echo $numbers[3];
+    echo "<br>";
+    echo $numbers[4];
+    echo "<br>";
+    echo $numbers[5];
+    echo "<br>";
+    $mixed = array(6, "rebane", "koer", 5.5, array("x", "y", "z"));
+    echo $mixed [2];
+    echo "<br>";
+    echo $mixed [4][2];
+    echo "<br>";
+	?>
+	<?php 
+	$mixed[2] = "kass"; 
+	$mixed[5] = "hiir";
+	$mixed[] = "hobune";
+	?>
+	<pre><?php echo print_r($mixed); ?></pre>
+
+	<h2>Associative arrays</h2>
+	<?php $assoc = array("first_name" => "Merilin", "last_name" => "Metsamaa"); 
+	echo $assoc ["first_name"];
+	echo "<br>";
+	echo $assoc ["last_name"];
+    echo "<br>";
+
+	?>
+	<?php $numbers = array(0 => 4, 5 => 8, 6 => 15, 9 => 16, 10 => 23, 15 => 42);
+	$assoc["age"] = 25;
+	$assoc["first_name"] = "Anne";
+    $assoc["e_mail"] = "merilin.metsamaa@khk.ee";
+    echo "Minu nimi on {$assoc["first_name"]} {$assoc["last_name"]}";
+    echo ". Minu e-post on {$assoc["e_mail"]}."; 
+    
+    ?>
+    
 </body>
 </html>
